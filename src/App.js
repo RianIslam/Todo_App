bd246629b8430b4d770c12d2978b6b7b74880e6a
RianIslam <rianislamdr@gmail.com>
2021-01-17 21:55:47 +0600

    add todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import React from 'react'
+
 
 function App() {
+
+  const [toDoList,setToDoList] = useState([
+    {text : 'Mow the law'},
+    {text : 'Wash the car'},
+    {text : 'Take out the trash'}
+  ]);
+
+
+  const [value,setValue] =useState('');
+
+  const handleSubmit = (mouseClick) => {
+    mouseClick.preventDefault();
+    addToDo(value);
+    setValue('');
+  };
+
+
+
+  const addToDo = (text) =>{
+    const updatedToDoList = [...toDoList, {text}];
+    setToDoList(updatedToDoList);
+  }
+
+
+  const handleDelete= (todo) =>{
+   const fulterToDoList = toDoList.filter(currentToDoListValue => (currentToDoListValue !== todo));
+   setToDoList(fulterToDoList);
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="toDoList">
+          {
+            toDoList.map((todo,index)=>(
+              <span key={index}>
+                <p>{todo.text}</p>
+                <button onClick={()=>handleDelete(todo)}>Delete</button>
+
+              </span>
+            ))
+          }
+      </div>
+      <div className="toDoInput">
+          <form onSubmit={handleSubmit}>
+            <input 
+              placeholder='Type To-Do Here'
+              value={value}
+              onChange={keyboardStoke => setValue (keyboardStoke.target.value)}
+            />
+            <button type='submit'>Add TODO</button>
+          </form>
+      </div>
+      
     </div>
   );
 }
